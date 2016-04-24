44addea71ec0080d775a3e529ab17a17fef79382
AtanasNaydenov <AtanasNaydenov@outlook.com>
2016-04-24 18:07:43 +0200

    homepage update + entryapp (not finished)

---
+++ Applications/EntryApp/WAvisitorCheck/DBHelper.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WAvisitorCheck
+{
+    public class DBHelper
+    {
+        public MySqlConnection connection;
+
+        public DBHelper()
+        {
+            String connectionInfo = "server=localhost;" +
+                                    "database=propdbtest;" +
+                                    "user id=root;" +
+                                    "password=;" +
+                                    "connect timeout=30;" +
+                                    "convert zero datetime=True";
+
+            connection = new MySqlConnection(connectionInfo);
+        }
+        public int CheckIfPaid(string bracelet_id)
+        {
+            String sql = "SELECT PAID FROM visitors WHERE BRACELET_ID = "+ "\"" + bracelet_id + "\"";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+
+            int paid = -1 ;
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    paid = Convert.ToInt32(reader["PAID"]);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return paid;
+        }
+
+        public List<Visitor> GetAllVisitors()
+        {
+            String sql = "SELECT * FROM visitors";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+
+            List<Visitor> temp;
+            temp = new List<Visitor>();
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                int u_id;
+                string pass;
+                string mail;
+                string fnm;
+                string lnm;
+                DateTime db;
+                double blnc;
+                bool pay;
+                string b_id;
+                int c_id;
+                int st;
+
+                while (reader.Read())
+                {
+                    if (reader["user_id"] == DBNull.Value) { u_id= 0; }
+                    else { u_id = Convert.ToInt32(reader["user_id"]); }
+
+                    pass = Convert.ToString(reader["password"]);
+
+                    mail = Convert.ToString(reader["email"]);
+
+                    fnm = Convert.ToString(reader["fname"]);
+
+                    lnm = Convert.ToString(reader["lname"]);
+
+                    db =Convert.ToDateTime(reader["dob"]);
+
+                    blnc = Convert.ToDouble(reader["balance"]);
+
+                    pay = Convert.ToBoolean(reader["paid"]);
+
+                    if(reader["bracelet_id"] == DBNull.Value) { b_id = null; }
+                   else{ b_id = Convert.ToString(reader["bracelet_id"]); }
+
+                    if (reader["camping_id"] == DBNull.Value) { c_id = 0; }
+                    else { c_id = Convert.ToInt32(reader["camping_id"]); }
+
+                    st = Convert.ToInt32(reader["camping_id"]);
+
+                    temp.Add(new Visitor(u_id, pass, mail, fnm, lnm, db, blnc, pay, b_id, c_id,st));
+                }
+            }
+            catch(Exception ex )
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return temp;
+        }
+
+        public int GetStatus(string bracelet_id)
+        {
+            int status = -1;
+
+            String sql = "SELECT STATUS FROM visitors WHERE BRACELET_ID = " + "\"" + bracelet_id + "\"";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    status = Convert.ToInt32(reader["STATUS"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return status;
+        }
+        
+        public void UpdateStatus(string bracelet_id)
+        {
+            String sql;
+            MySqlCommand command;
+
+            if (GetStatus(bracelet_id) == 1)
+            {
+                sql = "UPDATE visitors SET STATUS=0 WHERE BRACELET_ID = " + "\"" + bracelet_id + "\"";
+                command = new MySqlCommand(sql, connection);
+
+
+            }
+            else if (GetStatus(bracelet_id) == 0)
+            {
+
+            }
+            else { MessageBox.Show("NOT A VALID BRACELET"); }
+        }
+
+
+
+    }
+}
---
+++ Applications/EntryApp/WAvisitorCheck/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Phidgets;
+using Phidgets.Events;
+
+namespace WAvisitorCheck
+{
+    public partial class Form1 : Form
+    {
+        private RFID myRFIDReader;
+        private DBHelper dbh;
+        public Form1()
+        {
+            InitializeComponent();
+            dbh = new DBHelper();
+            try
+            {
+                myRFIDReader = new RFID();
+                myRFIDReader.Tag += new TagEventHandler(ProcessThisTag);
+                listBox1.Items.Add("WELCOME");
+            }
+            catch (PhidgetException) { listBox1.Items.Add("error at start-up."); }
+            pictureBox1.BackColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myRFIDReader.open();
+                myRFIDReader.waitForAttachment(3000);
+                listBox1.Items.Add("an RFID-reader is found and opened.");
+                myRFIDReader.Antenna = true;
+                myRFIDReader.LED = true;
+            }
+            catch (PhidgetException) { listBox1.Items.Add("no RFID-reader opened."); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myRFIDReader.LED = false;
+            myRFIDReader.Antenna = false;
+            myRFIDReader.close();
+            listBox1.Items.Add("an RFID-reader is closed.");
+        }
+        private void ProcessThisTag(object sender, TagEventArgs e)
+        {
+            //listBox1.Items.Add("rfid has tag-nr: " + e.Tag);
+           int paid = dbh.CheckIfPaid(e.Tag);
+            if (paid == 1)
+            {
+                pictureBox1.BackColor = Color.Green;
+                //Start timer, your program continues execution normaly
+                timer1.Start();
+                //If you use sleep(2000) your program stop working for two seconds.
+                //listBox1.Items.Add(paid.ToString());
+
+
+            }
+            else if(paid==0)
+            {
+                pictureBox1.BackColor = Color.Red;
+                MessageBox.Show("This person has not payed");
+            }
+            else
+            {
+                MessageBox.Show("this rfid is not registered");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pictureBox1.BackColor = Color.Red;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           List<Visitor> temp = dbh.GetAllVisitors();
+            foreach(Visitor visitor in temp)
+            {
+                listBox1.Items.Add(visitor.getInfo());
+            }
+        }
+
+
+    }
+}
