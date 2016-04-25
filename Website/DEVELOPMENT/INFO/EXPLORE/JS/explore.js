81de50d1c2524dc11de130514f07e32352fb2a9e
dee-me-tree-or-love <dmitry-orlov@hotmail.com>
2016-04-25 13:53:24 +0200

    A LOOOOT OF CHANGES, website version 0.4
    damn, son, where did you find this?
    please, don't mess with the files ;)

--- Website/DEVELOPMENT/INFO/EXPLORE/JS/explore.js
+++ Website/DEVELOPMENT/INFO/EXPLORE/JS/explore.js
@@ -2,4 +2,27 @@ $(document).ready(function() {
 	// first page load
 	// to load up the header
 	$( "#header").load( "../default/headerstatic.html" );
+	// to manage opening and closing the banner
+	// $(".closedfirst").hide();
+	// $(".closedsecond").hide();
+	
+	// $( ".central" ).parent().click(function() {
+			// $(".banner").hide();
+	// });
+	$( ".x" ).click(function() {
+		$(".banner").hide();
+	});
+	$( "#imgmap" ).click(function() {
+		$(".banner").show();
+	});
 });
+
+
+// $(window).on("scroll", function () {
+	// if ($(this).scrollTop() > 700) {
+		// $(".closedfirst").show("slow");
+	// }
+	// if ($(this).scrollTop() > 1400) {
+		// $(".closedsecond").show("slow");
+	// }
+// });
