dedbf75afc36c9b4f587f30df28137ae6a5dada4
dmarkov00 <dmarkov23@gmail.com>
2016-05-08 15:58:31 +0200

    Uploading the personal page

---
+++ Website/personal-page/click-html/bracelet.php
@@ -0,0 +1,51 @@
+<?php
+require_once '../php/dbconfig.php';
+require_once '../php/data.php';
+ ?>
+<div class="row"> </div>
+<div class="row">
+  <div class="col-xs-8">
+    <p class="object-info-bracelet">
+      Status:
+    </p>
+  </div>
+  <div class="col-xs-4">
+    <p class="object-text-bracelet">
+      <?php echo $status ?>
+    </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-6">
+    <p class="object-info-bracelet">
+      Type:
+    </p>
+  </div>
+  <div class="col-xs-6">
+    <p class="object-text-bracelet">
+    Best
+    </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-6">
+    <p class="object-info-bracelet">
+      Color:
+    </p>
+  </div>
+  <div class="col-xs-6">
+    <p class="object-text-bracelet">
+      Dark blue shit
+    </p>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-xs-3 col-xs-offset-9">
+    <a href="#/">
+        <p id="bracelet-close-button" class="object-text-general">
+            close
+        </p>
+    </a>
+  </div>
+</div>
