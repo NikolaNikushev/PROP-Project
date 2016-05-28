454265a5f89112e05424785e3d74a903aa266b41
dmarkov00 <dmarkov23@gmail.com>
2016-05-28 15:29:04 +0200

    Activity entrance-working functionality

---
+++ Applications/ActivityEntrance/ActivityEntrance/RFIDHelper.cs
@@ -0,0 +1,32 @@
+﻿using Phidgets;
+using Phidgets.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityEntrance
+{
+    public class RFIDHelper
+    {
+        private RFID myRFIDReader;
+        //public void InitialiseRFID()
+        //{
+        //    myRFIDReader = new RFID();
+        //    myRFIDReader.Attach += new AttachEventHandler(ShowWhoIsAttached);
+        //    myRFIDReader.Detach += new DetachEventHandler(ShowWhoIsDetached);
+        //    myRFIDReader.Tag += new TagEventHandler(ProcessThisTag);
+
+        //}
+        //private void ShowWhoIsAttached(object sender, AttachEventArgs e)
+        //{
+        //    listBox1.Items.Add("RFIDReader attached!, serial nr: " + e.Device.SerialNumber.ToString());
+        //}
+        //private void ShowWhoIsDetached(object sender, DetachEventArgs e)
+        //{
+        //    listBox1.Items.Add("RFIDReader detached!, serial nr: " + e.Device.SerialNumber.ToString());
+        //}
+
+    }
+}
