9b606bffbe28a96be3f84fe58b349b1788ed8dc4
AtanasNaydenov <AtanasNaydenov@outlook.com>
2016-06-04 07:52:32 +0200

    camping entry app functionality is on spot

---
+++ Applications/CampingEntryApp/CampingEntryApp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WAvisitorCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnAddDay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMode = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 452);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(150, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(616, 436);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(772, 37);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 37);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(772, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnAddDay
+            // 
+            this.btnAddDay.Location = new System.Drawing.Point(776, 189);
+            this.btnAddDay.Name = "btnAddDay";
+            this.btnAddDay.Size = new System.Drawing.Size(90, 53);
+            this.btnAddDay.TabIndex = 4;
+            this.btnAddDay.Text = "Add Day";
+            this.btnAddDay.UseVisualStyleBackColor = true;
+            this.btnAddDay.Click += new System.EventHandler(this.btnAddDay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "MODE: ";
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Location = new System.Drawing.Point(199, 9);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(0, 17);
+            this.lblMode.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 525);
+            this.Controls.Add(this.lblMode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddDay);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnAddDay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMode;
+    }
+}
+
---
+++ Applications/CampingEntryApp/CampingEntryApp/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Phidgets;
+using Phidgets.Events;
+
+namespace WAvisitorCheck
+{
+    public partial class Form1 : Form
+    {
+        private RFID myRFIDReader;
+        private DBHelper dbh;
+        public Form1()
+        {
+            InitializeComponent();
+            dbh = new DBHelper();
+            try
+            {
+                myRFIDReader = new RFID();
+                myRFIDReader.Tag += new TagEventHandler(ProcessThisTag);
+                listBox1.Items.Add("WELCOME");
+            }
+            catch (PhidgetException) { listBox1.Items.Add("error at start-up."); }
+            pictureBox1.BackColor = Color.White;
+            lblMode.Text = "Entry";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myRFIDReader.open();
+                myRFIDReader.waitForAttachment(3000);
+                listBox1.Items.Add("an RFID-reader is found and opened.");
+                myRFIDReader.Antenna = true;
+                myRFIDReader.LED = true;
+            }
+            catch (PhidgetException) { listBox1.Items.Add("no RFID-reader opened."); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myRFIDReader.LED = false;
+            myRFIDReader.Antenna = false;
+            myRFIDReader.close();
+            listBox1.Items.Add("an RFID-reader is closed.");
+        }
+        private void ProcessThisTag(object sender, TagEventArgs e)
+        {
+            //listBox1.Items.Add("rfid has tag-nr: " + e.Tag);
+            //some fields
+            int paid = dbh.CheckIfPaid(e.Tag);
+            int user_id = dbh.GetUserID(e.Tag);
+            int camping_id = dbh.GetCampingID(e.Tag);
+            DateTime today = DateTime.Today;
+            DateTime arrivalDate = dbh.GetArrivalDate(user_id);
+            
+
+            if (paid == 1)
+            {
+                if (camping_id != 0)
+                {
+                    if (arrivalDate <= today)
+                    {
+                        dbh.UpdateLocation(user_id.ToString());
+                        pictureBox1.BackColor = Color.Green;
+                        //Start timer, your program continues execution normaly
+                        timer1.Start();
+                        //If you use sleep(2000) your program stop working for two seconds.
+                        //listBox1.Items.Add(paid.ToString());
+                        //dbh.UpdateStatus(e.Tag);
+                    }
+                    else {
+                        pictureBox1.BackColor = Color.Red;
+                        //Start timer, your program continues execution normaly
+                        timer1.Start();
+                        AutoClosingMessageBox.Show("Sorry, you will have to wait until "+arrivalDate+" to enter", "capiton", 1000);
+                    }
+                }
+                else
+                {
+                    pictureBox1.BackColor = Color.Red;
+                    //Start timer, your program continues execution normaly
+                    timer1.Start();
+                    AutoClosingMessageBox.Show("Sorry, you will have to try to book a tent to enter", "capiton", 1000);
+
+                }
+
+            }
+            else if(paid==0)
+            {
+                pictureBox1.BackColor = Color.Red;
+                //Start timer, your program continues execution normaly
+                timer1.Start();
+                //If you use sleep(2000) your program stop working for two seconds.
+                //listBox1.Items.Add(paid.ToString());
+                AutoClosingMessageBox.Show("This person has not payed for the event - KICK HIM OUT!","capiton",1000);
+            }
+            else
+            {
+                pictureBox1.BackColor = Color.Red;
+                //Start timer, your program continues execution normaly
+                timer1.Start();
+                //If you use sleep(2000) your program stop working for two seconds.
+                //listBox1.Items.Add(paid.ToString());
+                AutoClosingMessageBox.Show("this rfid is not registered - KICK THIS PERSON OUT!", "capiton", 1000);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pictureBox1.BackColor = Color.White;
+        }
+
+        public void AddMoneyTag(object sender, TagEventArgs e)
+        {
+            
+            string bracelet_id = e.Tag;
+            int paid = dbh.CheckIfPaid(e.Tag);
+            int user_id = dbh.GetUserID(e.Tag);
+            int camping_id = dbh.GetCampingID(e.Tag);
+            DateTime today = DateTime.Today;
+            DateTime arrivalDate = dbh.GetArrivalDate(user_id);
+
+            if (paid == 1)
+            {
+                if (camping_id != 0)
+                {
+                    dbh.PayForDay(bracelet_id, camping_id);
+                }
+                else
+                {
+                    AutoClosingMessageBox.Show("Sorry, you will have to try to book a tent to enter", "capiton", 2000);
+
+                }
+
+            }
+            else if (paid == 0)
+            {
+                AutoClosingMessageBox.Show("This person has not payed for the event - KICK HIM OUT!", "capiton", 2000);
+            }
+            else
+            {
+                AutoClosingMessageBox.Show("this rfid is not registered - KICK THIS PERSON OUT!", "capiton", 2000);
+            }
+            myRFIDReader.Tag -= new TagEventHandler(AddMoneyTag);
+            myRFIDReader.Tag += new TagEventHandler(ProcessThisTag);
+            lblMode.Text = "Entry";
+        }
+        private void btnAddDay_Click(object sender, EventArgs e)
+        {
+            myRFIDReader.Tag -= new TagEventHandler(ProcessThisTag);
+            myRFIDReader.Tag += new TagEventHandler(AddMoneyTag);
+            AutoClosingMessageBox.Show("Scan your RFID", "Nasko e Sexy",2000);
+            lblMode.Text = "Adding a day";
+
+        }
+
+    }
+}
