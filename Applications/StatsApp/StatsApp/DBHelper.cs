933493642fecd3706554a2fc3aaa0d05bd8b43ac
Dmitry <dmitry-orlov@hotmail.com>
2016-06-13 00:59:26 +0200

    New Stats App organisation

---
+++ Applications/StatsApp/StatsApp/DBHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace StatsApp
+{
+    public class DBHelper
+    {
+        public MySqlConnection connection;
+
+        public DBHelper()
+        {
+            String connectionInfo = "server=localhost;" +
+                                    "database=propdbtest;" +
+                                    "user id=root;" +
+                                    "password=;" +
+                                    "connect timeout=30;" +
+                                    "convert zero datetime=True";
+
+            connection = new MySqlConnection(connectionInfo);
+        }
+
+        public int GetNrCurrentVisitors()
+        {
+            int currentvisitors = -1;
+            String sql = "SELECT COUNT(*) AS COUNT FROM visitors WHERE STATUS = 1";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    currentvisitors = Convert.ToInt32(reader["COUNT"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return currentvisitors;
+        }
+    }
+}
---
+++ Applications/StatsApp/StatsApp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatsApp
+{
+    public partial class Form1 : Form
+    {
+        DBHelper dbh;
+        BinaryFileHelper bfh = new BinaryFileHelper();
+        int time;
+        string date;
+        public Form1()
+        {
+            InitializeComponent();
+            dbh = new DBHelper();
+
+            label2.Text = dbh.GetNrCurrentVisitors().ToString();
+            timer1.Start();
+
+            timer2.Interval = 5000 /*(uncomment for an hourly interval * 60 * 60 */;
+            time = System.DateTime.Now.Hour;
+            date = DateTime.Now.ToString("dd");
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = dbh.GetNrCurrentVisitors().ToString();
+            if ((Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy")) == "12.06.2016" || Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy")) == "13.06.2016" || Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy")) == "27.04.2016") && timer2.Enabled != true)
+            {
+                timer2.Start();
+                listBox1.Items.Add("The date is " + Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy")));
+                listBox1.Items.Add(Convert.ToString(System.DateTime.Now.Hour) + "    visitors: " + dbh.GetNrCurrentVisitors().ToString());
+
+                var visitorsList = listBox1.Items.Cast<String>().ToList();
+                bfh.SaveToBinaryFile(visitorsList);
+
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            string newdate = DateTime.Now.ToString("dd");
+            if (date != newdate)
+            {
+                listBox1.Items.Clear();
+                date = DateTime.Now.ToString("dd");
+                listBox1.Items.Add("The date is " + Convert.ToString(DateTime.Now.ToString("dd/MM/yyyy")));
+            }
+
+            int newtime = System.DateTime.Now.Hour;
+            if (newtime != time)
+            {
+                listBox1.Items.Add(Convert.ToString(System.DateTime.Now.Hour) + "    visitors: " + dbh.GetNrCurrentVisitors().ToString());
+
+                if (time < 23)
+                {
+                    time++;
+                }
+                else
+                { time = 0; }
+
+                var visitorsList = listBox1.Items.Cast<String>().ToList();
+                bfh.SaveToBinaryFile(visitorsList);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            //my mistake
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            List<string> data = bfh.LoadFromBinaryFile("../../DAY_25.04.2016");
+            foreach (string a in data)
+            {
+                listBox2.Items.Add(a);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            List<string> data = bfh.LoadFromBinaryFile("../../DAY_26.04.2016");
+            foreach (string a in data)
+            {
+                listBox2.Items.Add(a);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            List<string> data = bfh.LoadFromBinaryFile("../../DAY_27.04.2016");
+            foreach (string a in data)
+            {
+                listBox2.Items.Add(a);
+            }
+        }
+    }
+}
