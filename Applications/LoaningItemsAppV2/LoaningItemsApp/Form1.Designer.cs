ff7566ed0707e864634cfcdd08192410e85d62bc
Kondio <hristian_v@yahoo.com>
2016-06-26 04:08:57 +0200

    Replacing the old loaningitemsapp with the Version2

--- Applications/LoaningItemsAppV2/LoaningItemsApp/Form1.Designer.cs
+++ Applications/LoaningItemsAppV2/LoaningItemsApp/Form1.Designer.cs
@@ -30,14 +30,21 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.txtBoxPrice = new System.Windows.Forms.TextBox();
             this.lBoxLoanedItems = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLoan = new System.Windows.Forms.Button();
             this.lBoxItemsForLoan = new System.Windows.Forms.ListBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.lBoxReturnedItems = new System.Windows.Forms.ListBox();
@@ -54,31 +61,93 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(784, 566);
+            this.tabControl1.Size = new System.Drawing.Size(1321, 674);
             this.tabControl1.TabIndex = 1;
+            this.tabControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tabControl1_MouseClick);
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.btnLoad);
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.txtBoxPrice);
             this.tabPage1.Controls.Add(this.lBoxLoanedItems);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.dateTimePicker1);
-            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.btnLoan);
             this.tabPage1.Controls.Add(this.lBoxItemsForLoan);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(776, 540);
+            this.tabPage1.Size = new System.Drawing.Size(1313, 648);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(694, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 32);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(694, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Close reader";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(691, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Your balance:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(792, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(567, 60);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(95, 32);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load Items";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(477, 113);
+            this.label2.Location = new System.Drawing.Point(703, 113);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 4;
@@ -86,7 +155,7 @@
             // 
             // txtBoxPrice
             // 
-            this.txtBoxPrice.Location = new System.Drawing.Point(556, 108);
+            this.txtBoxPrice.Location = new System.Drawing.Point(792, 110);
             this.txtBoxPrice.Name = "txtBoxPrice";
             this.txtBoxPrice.Size = new System.Drawing.Size(100, 20);
             this.txtBoxPrice.TabIndex = 2;
@@ -94,15 +163,15 @@
             // lBoxLoanedItems
             // 
             this.lBoxLoanedItems.FormattingEnabled = true;
-            this.lBoxLoanedItems.Location = new System.Drawing.Point(316, 252);
+            this.lBoxLoanedItems.Location = new System.Drawing.Point(8, 437);
             this.lBoxLoanedItems.Name = "lBoxLoanedItems";
-            this.lBoxLoanedItems.Size = new System.Drawing.Size(197, 186);
+            this.lBoxLoanedItems.Size = new System.Drawing.Size(521, 160);
             this.lBoxLoanedItems.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 186);
+            this.label1.Location = new System.Drawing.Point(553, 186);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 3;
@@ -110,40 +179,50 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(428, 180);
+            this.dateTimePicker1.Location = new System.Drawing.Point(694, 179);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 2;
             // 
-            // button1
+            // btnLoan
             // 
-            this.button1.Location = new System.Drawing.Point(300, 101);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Loan item";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLoan.Location = new System.Drawing.Point(567, 113);
+            this.btnLoan.Name = "btnLoan";
+            this.btnLoan.Size = new System.Drawing.Size(95, 32);
+            this.btnLoan.TabIndex = 1;
+            this.btnLoan.Text = "Loan item";
+            this.btnLoan.UseVisualStyleBackColor = true;
+            this.btnLoan.Click += new System.EventHandler(this.btnLoan_Click);
             // 
             // lBoxItemsForLoan
             // 
             this.lBoxItemsForLoan.FormattingEnabled = true;
-            this.lBoxItemsForLoan.Location = new System.Drawing.Point(36, 60);
+            this.lBoxItemsForLoan.Location = new System.Drawing.Point(8, 60);
             this.lBoxItemsForLoan.Name = "lBoxItemsForLoan";
-            this.lBoxItemsForLoan.Size = new System.Drawing.Size(227, 381);
+            this.lBoxItemsForLoan.Size = new System.Drawing.Size(521, 355);
             this.lBoxItemsForLoan.TabIndex = 0;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.listBox1);
             this.tabPage2.Controls.Add(this.label3);
             this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Controls.Add(this.lBoxReturnedItems);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(776, 540);
+            this.tabPage2.Size = new System.Drawing.Size(1313, 648);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(557, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(381, 355);
+            this.listBox1.TabIndex = 3;
             // 
             // label3
             // 
@@ -162,6 +241,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Return Item";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // lBoxReturnedItems
             // 
@@ -175,7 +255,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 566);
+            this.ClientSize = new System.Drawing.Size(1321, 674);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -198,11 +278,18 @@
         private System.Windows.Forms.ListBox lBoxLoanedItems;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLoan;
         private System.Windows.Forms.ListBox lBoxItemsForLoan;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox lBoxReturnedItems;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
---
+++ Applications/LoaningItemsAppV2/LoaningItemsApp/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Phidgets;
+using Phidgets.Events;
+
+namespace LoaningItemsApp
+{
+    public partial class Form1 : Form
+    {
+        
+        DBHelper dbh;
+        private RFID RFIDReader;
+        LoanShopp JobsDone = new LoanShopp("Job's Done");
+        double balance;
+        string tag;
+        double totalprice;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = JobsDone.Name;
+            dbh = new DBHelper();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RFIDReader = new RFID();
+                RFIDReader.open();
+                RFIDReader.waitForAttachment(4000);
+                RFIDReader.Antenna = true;
+                RFIDReader.LED = true;
+                RFIDReader.Tag += new TagEventHandler(ProcessThisTag);
+                
+
+
+            }
+            catch(PhidgetException)
+            {
+                MessageBox.Show("Couldn't open RFID reader");
+            }
+        }
+        private void ProcessThisTag(object sender, TagEventArgs e)
+        {
+            int paid = dbh.CheckIfPaid(e.Tag);
+            if (paid == 1)
+            {
+
+                
+                balance = dbh.getBalance(e.Tag);
+                textBox1.Text = Convert.ToString(balance);
+                tag = e.Tag;
+               
+                
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid tag");
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            foreach(Item i in JobsDone.LoanItems)
+            {
+                lBoxItemsForLoan.Items.Add(i.AsString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try {
+                RFIDReader.LED = false;
+                RFIDReader.Antenna = false;
+                RFIDReader.close();
+            }
+            catch
+            {
+                MessageBox.Show("You haven't opened the reader yet");
+            }
+        }
+
+        private void btnLoan_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to loan this item?", "Message", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                try {
+                    Item i;
+                    int index = lBoxItemsForLoan.SelectedIndex;
+                    i = JobsDone.LoanItems[index];
+                    if (i.Loaned == 1)
+                    {
+                        MessageBox.Show("Item is alredy loaned out");
+                    }
+                    else
+                    {
+                        balance = dbh.getBalance(tag);
+                        DateTime startdate = DateTime.Now;
+                       
+                        dbh.SetStartDate(i.Article_id, startdate);
+                        dbh.SetLoanStatus(i.Article_id);
+                        i.Loaned = 1;
+                        lBoxItemsForLoan.Items.Remove(i);
+                        lBoxLoanedItems.Items.Add(i.AsString());
+                        DateTime duedate = dateTimePicker1.Value.Date;
+                        i.Returndate = duedate;
+                        int totaldays = (duedate - startdate).Days;
+                        if (totaldays == 0)
+                        {
+                            totalprice = i.Priceperday;
+                            txtBoxPrice.Text = totalprice.ToString();
+                        }
+                        else {
+                            int ppday = i.Priceperday;
+                            totaldays++;
+                            totalprice = ppday * totaldays;
+                            txtBoxPrice.Text = totalprice.ToString();
+                        }
+                        double newbalance = balance - totalprice;
+                        dbh.UpdateBalance(tag, newbalance);
+                        textBox1.Text = Convert.ToString(newbalance);
+
+                    }
+                }
+                catch(ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show("You have not selected an item");
+                }
+            }
+            else if(dialogResult == DialogResult.No)
+            {
+                lBoxItemsForLoan.SelectedIndex = -1;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lBoxItemsForLoan.Items.Clear();
+            lBoxLoanedItems.Items.Clear();
+            txtBoxPrice.Clear();
+            lBoxItemsForLoan.SelectedIndex = -1;
+            dateTimePicker1.ResetText();
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Item var;
+            int index = listBox1.SelectedIndex;
+            var = JobsDone.LoanItems[index];
+            int articleId = var.Article_id;
+            dbh.SetLoanStatusToFalse(articleId);
+            
+            listBox1.Items.Remove(index);
+            lBoxReturnedItems.Items.Add(var.AsString());
+        }
+
+        
+
+        private void tabControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            foreach (Item i in JobsDone.LoanItems)
+            {
+                listBox1.Items.Add(i.AsString());
+            }
+        }
+    }
+
+}
+
+
