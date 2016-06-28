aa3faf53f1985c3245f5c196b133f3726d004747
Dmitry <dmitry-orlov@hotmail.com>
2016-06-28 06:41:37 +0200

    commit

--- Applications/DBConnectionDll/DBConnectionDll/Connection.cs
+++ Applications/DBConnectionDll/DBConnectionDll/Connection.cs
@@ -5,10 +5,10 @@ namespace DBConnectionDll
 {
     static public class Connection
     {
-        public const String connectionInfo = "server=localhost;" +
-                                    "database=propdbtest;" +
-                                    "user id=root;" +
-                                    "password=;" +
+        public const String connectionInfo = "server=athena01.fhict.local;" +
+                                    "database=dbi339282;" +
+                                    "user id=dbi339282;" +
+                                    "password=bccMxqtHck;" +
                                     "connect timeout=30;" +
                                     "convert zero datetime=True";
 
