9c10015dbf6660d2891c268e5768ed65747d514e
dmarkov00 <dmarkov23@gmail.com>
2016-06-30 20:03:18 +0200

    Application runner submit

---
+++ Presentation/ApplicationRunner/ApplicationRunner/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ApplicationRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ApplicationsListBox = new System.Windows.Forms.ListBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ApplicationsListBox
+            // 
+            this.ApplicationsListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ApplicationsListBox.Font = new System.Drawing.Font("Tekton Pro Ext", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ApplicationsListBox.FormattingEnabled = true;
+            this.ApplicationsListBox.ItemHeight = 28;
+            this.ApplicationsListBox.Location = new System.Drawing.Point(1, 5);
+            this.ApplicationsListBox.Name = "ApplicationsListBox";
+            this.ApplicationsListBox.Size = new System.Drawing.Size(321, 224);
+            this.ApplicationsListBox.TabIndex = 0;
+            // 
+            // RunButton
+            // 
+            this.RunButton.BackColor = System.Drawing.SystemColors.Window;
+            this.RunButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RunButton.Font = new System.Drawing.Font("Tekton Pro Ext", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RunButton.Location = new System.Drawing.Point(1, 225);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(321, 33);
+            this.RunButton.TabIndex = 1;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = false;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 262);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.ApplicationsListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Application Runner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ApplicationsListBox;
+        private System.Windows.Forms.Button RunButton;
+    }
+}
+
---
+++ Presentation/ApplicationRunner/ApplicationRunner/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApplicationRunner
+{
+    public partial class Form1 : Form
+    {
+        string[] appNames = { "Bracelet management", "Event entrance", "Camping entrance", "Shop", "Statistics", "Visitor support" };
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < appNames.Length; i++)
+            {
+                ApplicationsListBox.Items.Add(appNames[i]);
+            }
+            //adding some styles to the button
+            RunButton.TabStop = false;
+            RunButton.FlatStyle = FlatStyle.Flat;
+            RunButton.FlatAppearance.BorderSize = 0;
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            var root = System.IO.Directory.GetCurrentDirectory();
+            switch (ApplicationsListBox.SelectedItem.ToString())
+            {
+                //"../../../../../Applications/BraceletManagement/BraceletManagement/bin/Debug/BraceletManagement.exe"
+                case "Bracelet management":
+                    Process.Start(root + "/../../../../../Applications/BraceletManagement/BraceletManagement/bin/Debug/BraceletManagement.exe");
+                    break;
+                case "Event entrance":
+                    Process.Start(root + "/../../../../../Applications/EntryApp/WAvisitorCheck/bin/Debug/WAvisitorCheck.exe");
+                    break;
+                case "Camping entrance":
+                    Process.Start(root + "/../../../../../Applications/CampingEntryApp/CampingEntryApp/bin/Debug/CampingEntryApp.exe");
+                    break;
+                case "Shop":
+                    Process.Start(root + "/../../../../../Applications/ShopAppStable/ShopApp/bin/Debug/ShopApp.exe");
+                    break;
+                case "Statistics":
+                    Process.Start(root + "/../../../../../Applications/StatsApp/StatsApp/bin/Debug/StatsApp.exe");
+                    break;
+                case "Visitor support":
+                    Process.Start(root + "/../../../../../Applications/VisSup/BraceletManagement/bin/Debug/BraceletManagement.exe");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
